5c43b38f7340c8727e84d42eb3601a825243678b
Viacheslav Zenkovich <vzenkovich@setronica.com>
2022-05-04 16:40:05 +0700

    Revert "feat: Made it clear that we stand with Ukraine"
    This reverts commit 57c20b9f96c6fdf7664a5fc6b6c9a677f819b99d.

--- variables.tf
+++ variables.tf
@@ -515,9 +515,3 @@ variable "security_group_use_name_prefix" {
   type        = bool
   default     = true
 }
-
-variable "putin_khuylo" {
-  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
-  type        = bool
-  default     = true
-}
